580e71b5e8d411c335564964a11571690d8d9fd8
shivakrishna <vemulashiva55@gmail.com>
2020-06-20 22:33:51 +0530

    Remove duplicated directory

--- Ex_Files_Django_Forms/Exercise Files/01_04_end/nandiasgarden-project/pizza/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'pizza/home.html')
-
-
-def order(request):
-    return render(request, 'pizza/order.html')
--- Ex_Files_Django_Forms/Exercise Files/01_06_begin/nandiasgarden-project/pizza/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .forms import PizzaForm
-
-def home(request):
-    return render(request, 'pizza/home.html')
-
-def order(request):
-    form = PizzaForm()
-    return render(request, 'pizza/order.html', {'pizzaform':form})
